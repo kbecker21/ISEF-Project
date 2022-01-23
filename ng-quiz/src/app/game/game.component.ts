73b1e85951e6576f1b24d02023700b8635c91684
kbecker21 <kevin.becker@iubh-fernstudium.de>
2022-01-23 16:33:39 +0100

    updated quiz (not done yet)

--- ng-quiz/src/app/game/game.component.ts
+++ ng-quiz/src/app/game/game.component.ts
@@ -3,6 +3,10 @@ import { Question } from '../shared/model/question.model';
 import { Answer } from '../shared/model/answer.model';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogComponent } from './dialog/dialog.component';
+import { QuizService } from '../shared/services/quiz.service';
+import { AuthService } from '../shared/services/auth.service';
+import { User } from '../shared/model/user.model';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -12,14 +16,19 @@ import { DialogComponent } from './dialog/dialog.component';
 })
 export class GameComponent implements OnInit {
 
+  loggedInUser: User = null;
+  userSub: Subscription = null;
+
   player1 = null;
   player2 = null;
 
   player1Points = 0;
   player2Points = 0;
 
-  question: Question = null;
-  answers: Answer[] = [];
+  questions: Question[] = [];
+
+  currentQuestion: Question = null;
+  currentAnswers: Answer[] = [];
 
   questionNumber = 0;
 
@@ -27,23 +36,67 @@ export class GameComponent implements OnInit {
   disableAnswerButton = false;
 
 
+  questionSub: Subscription = null;
+  answersSub: Subscription = null;
 
-  constructor(public dialog: MatDialog) { }
+
+  constructor(private auth: AuthService, public dialog: MatDialog, private quizService: QuizService) { }
 
   ngOnInit(): void {
+    this.userSub = this.auth.user.subscribe(user => {
+      this.loggedInUser = user;
+      this.player1 = this.loggedInUser;
+    });
+
     this.dummyCreateQuiz();
 
     this.dummyGetNewQuestion();
+
+    this.getQuiz();
+
+    this.getQuestion();
+
+    this.getAnswers();
+
+
+  }
+
+  getQuiz() {
+    //TODO: setzt Spieler, Kurs, Kategorie
+  }
+
+  getQuestion() {
+    const idSubject = 1;
+    const idCategory = 1;
+    this.questionSub = this.quizService.getQuestion(this.loggedInUser, idSubject, idCategory).subscribe(response => {
+      // TODO: 
+    },
+      errorMessage => {
+        console.log(errorMessage);
+      });
+
+
+    this.currentQuestion = this.questions[this.questionNumber];
+    this.questionNumber++;
+  }
+
+  getAnswers() {
+    const idQuestion = 1;
+    this.answersSub = this.quizService.getAnswers(this.loggedInUser, idQuestion).subscribe(response => {
+      this.currentAnswers = response;
+    },
+      errorMessage => {
+        console.log(errorMessage);
+      });
   }
 
   selectAnswer(answer: Answer) {
     if (answer.Truth) {
       this.openDialog(true, 'Super gemacht, weiter so.');
-      this.disableNextQuestionButton = false;
     } else {
       this.openDialog(false, 'Die richtige Antwort ist xy.');
-      this.disableNextQuestionButton = false;
     }
+    this.disableNextQuestionButton = false;
   }
 
   openDialog(isCorrect: boolean, answer: string) {
@@ -55,10 +108,15 @@ export class GameComponent implements OnInit {
     });
   }
 
-  dummyGetNewQuestion() {
-    this.questionNumber++;
 
-    this.question = {
+  nextQuestion() {
+    this.currentQuestion = this.questions[this.questionNumber];
+    this.questionNumber++;
+  }
+
+  dummyGetNewQuestion() {
+
+    let question1: Question = {
       idQuestion: 1,
       category_idcategory: 1,
       QuestionDescription: "Wer war der 44. Präsident der USA?",
@@ -66,35 +124,24 @@ export class GameComponent implements OnInit {
       CreateDate: ""
     }
 
-    const answer1: Answer = {
-      idAnswers: 1,
-      Question_idQuestion: 1,
-      Description: "Donald Trump",
-      Truth: 0
-    };
+    let question2: Question = {
+      idQuestion: 2,
+      category_idcategory: 2,
+      QuestionDescription: "Wer war der 1. Präsident der USA?",
+      Approved: 1,
+      CreateDate: ""
+    }
 
-    const answer2: Answer = {
-      idAnswers: 2,
-      Question_idQuestion: 1,
-      Description: "Barack Obama",
-      Truth: 1
-    };
 
-    const answer3: Answer = {
-      idAnswers: 3,
-      Question_idQuestion: 1,
-      Description: "Richard Nixon",
-      Truth: 0
-    };
+    let question3: Question = {
+      idQuestion: 3,
+      category_idcategory: 3,
+      QuestionDescription: "Wer war der 3. Präsident der USA?",
+      Approved: 1,
+      CreateDate: ""
+    }
 
-    const answer4: Answer = {
-      idAnswers: 4,
-      Question_idQuestion: 1,
-      Description: "George Bush",
-      Truth: 0
-    };
-
-    this.answers.push(answer1, answer2, answer3, answer4);
+    this.questions.push(question1, question2, question3);
   }
 
   dummyCreateQuiz() {
